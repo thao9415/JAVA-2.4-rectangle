9a50e243acaecd8f34875e4ee49e872a277e6a7a
thao9415 <thao.9415@gmail.com>
2019-09-30 15:31:17 +0700

    OOP rectangle

---
+++ src/Rectangle.java
@@ -0,0 +1,36 @@
+public class Rectangle {
+    private double width,height;
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public Rectangle(){
+    }
+    public Rectangle(double width,double height){
+        this.width=width;
+        this.height=height;
+    }
+
+    public double getArea(){
+        return this.height*this.width;
+    }
+    public double getPerimeter(){
+        return (this.height+this.width)*2;
+    }
+    public String display(){
+        return "Rectangle{" + "width=" + width + ", height=" + height + "}";
+    }
+}
